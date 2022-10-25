7bb58a7ec5863c3a6b799052bbd4c060ccaad2b1
Md Ikbal Hosen <mdiqbalhossain6463@gmail.com>
2022-10-25 15:40:24 +0600

    footer added

--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import { FaBeer } from 'react-icons/fa';
 const Footer = () => {
     return (
-        <div className='bg-success sticky p-5 mb-0'>
-            <h2>This is footer</h2>
+        <div className='bg-dark text-white sticky p-3 mb-0'>
+            <footer className='text-center'>
+                <small>All  copyright reserved &copy; Md Ikbal Hosen </small>
+            </footer>
+
         </div>
-    );
-};
+    )
+}
 
 export default Footer;
